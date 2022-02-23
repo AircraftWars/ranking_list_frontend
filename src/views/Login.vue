b7bfc2c203cf41f297cb6f1d9357e4cf16b9593e
OxOtn3 <oxotn3@foxmail.com>
2022-02-23 13:06:25 +0800

    initial commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
-<!--    <el-container>-->
-    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="ruleForm.username"></el-input>
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input v-model="ruleForm.password"></el-input>
-      </el-form-item>
+    <el-container>
+      <el-header>
+        <div style="font-size: 30px; color: #409EFF">
+          <a>AircraftWars 排行榜</a>
+        </div>
+      </el-header>
 
+      <el-main>
+        <el-table
+            :data="tableData"
+            stripe
+            style="width: 60%; margin-left: 320px">
+          <el-table-column
+              type="index"
+              width="180">
+          </el-table-column>
+          <el-table-column
+              prop="playerName"
+              label="玩家"
+              width="180">
+          </el-table-column>
+          <el-table-column
+              prop="score"
+              label="分数">
+          </el-table-column>
+          <el-table-column
+              prop="playTime"
+              label="日期">
+          </el-table-column>
+        </el-table>
+      </el-main>
 
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-      </el-form-item>
-    </el-form>
-<!--    </el-container>-->
+    </el-container>
   </div>
 </template>
 
@@ -23,52 +40,25 @@
 import request from "../utils/request";
 
   export default {
-    created() {
 
+    created() {
+      request.get("http://127.0.0.1:1234/api/getTopNRankingInfo", {
+        params: {
+          n: 10,
+        }
+      }).then(res => {
+        this.tableData = res.data;
+      })
     },
     data() {
-    return {
-      ruleForm: {
-        id: '1',
-        name: '',
-        password: '',
-      },
-      rules: {
-        username: [
-          { required: true, message: '请输入用户名', trigger: 'blur' },
-        ],
-        password: [
-          { required: true, message: '请输入密码', trigger: 'blur' },
-        ],
+      return {
+        tableData: []
       }
-    };
-  },
-  methods: {
-    submitForm(formName) {
-      const _this = this
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          // this.$axios.post('http://localhost:9090/login', this.ruleForm).then((res =>{
-          //   _this.$router.push("/message")
-          // }))
-
-          request.post("http://localhost:9090/user/login", this.ruleForm).then(res =>{
-            let mes = res['message']
-            console.log(res)
-            if(mes === '登陆成功'){
-              _this.$router.push("/message")
-            }
-          })
-
-        } else {
-          console.log('error submit!!');
-          return false;
-        }
-      });
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-    }
+
+  methods: {
+
+
   },
   name: "Login.vue"
 }
@@ -76,10 +66,5 @@ import request from "../utils/request";
 
 <style scoped>
 
-  .demo-ruleForm{
-    max-width: 500px;
-    margin-top: 300px;
-    margin-left: 550px;
-  }
 
 </style>
